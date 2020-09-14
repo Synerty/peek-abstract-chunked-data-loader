9cb92de2c09d37c7651d9bc14dd410c184907e3e
Jacob Sorrell <jacob.sorrell@synerty.com>
2020-09-15 09:22:50 +1000

    PEEK-671 Refactor ComponentLifeCycleEventEmitter class

--- peek_abstract_chunked_data_loader/private/admin-app.abctodo/status/status.component.ts
+++ peek_abstract_chunked_data_loader/private/admin-app.abctodo/status/status.component.ts
@@ -1,33 +1,30 @@
-import {Component} from "@angular/core";
-import {
-    ComponentLifecycleEventEmitter,
-    TupleDataObserverService,
-    TupleSelector
-} from "@synerty/vortexjs";
-import { BalloonMsgService } from "@synerty/peek-plugin-base-js"
-import {LoaderStatusTuple} from "../tuples/LoaderStatusTuple";
-
+import { Component } from "@angular/core"
+import { TupleDataObserverService, TupleSelector } from "@synerty/vortexjs"
+import { BalloonMsgService, NgLifeCycleEvents } from "@synerty/peek-plugin-base-js"
+import { LoaderStatusTuple } from "../tuples/LoaderStatusTuple"
 
 @Component({
-    selector: 'pl-pof-diagram-loader-loader-status',
-    templateUrl: './status.component.html'
+    selector: "pl-pof-diagram-loader-loader-status",
+    templateUrl: "./status.component.html"
 })
-export class StatusComponent extends ComponentLifecycleEventEmitter {
-
-    item: LoaderStatusTuple = new LoaderStatusTuple();
-
-    constructor(private balloonMsg: BalloonMsgService,
-                private tupleObserver: TupleDataObserverService) {
-        super();
-
+export class StatusComponent extends NgLifeCycleEvents {
+    
+    item: LoaderStatusTuple = new LoaderStatusTuple()
+    
+    constructor(
+        private balloonMsg: BalloonMsgService,
+        private tupleObserver: TupleDataObserverService
+    ) {
+        super()
+        
         let sub = this.tupleObserver.subscribeToTupleSelector(
             new TupleSelector(LoaderStatusTuple.tupleName, {})
-        ).subscribe((tuples: LoaderStatusTuple[]) => {
-            this.item = tuples[0];
-        });
-        this.onDestroyEvent.subscribe(() => sub.unsubscribe());
-
+        )
+            .subscribe((tuples: LoaderStatusTuple[]) => {
+                this.item = tuples[0]
+            })
+        this.onDestroyEvent.subscribe(() => sub.unsubscribe())
+        
     }
-
-
+    
 }

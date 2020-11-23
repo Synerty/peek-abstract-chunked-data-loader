f4ee21bba85e94859c39d95e2f0ba9d892141696
Jacob Sorrell <jacob.sorrell@synerty.com>
2020-11-23 10:13:58 +1000

    PEEK-745 Updated Server and Client entry hooks
    - Renamed mobile and desktop functions to field and office
    - Renamed ServerEntryHook to LogicEntryHook
    - Added peekOfficeEntryHook and peekFieldEntryHook

--- peek_abstract_chunked_data_loader/__init__.py
+++ peek_abstract_chunked_data_loader/__init__.py
@@ -1,5 +1,5 @@
 
-from peek_plugin_base.server.PluginServerEntryHookABC import PluginServerEntryHookABC
+from peek_plugin_base.server.PluginLogicEntryHookABC import PluginLogicEntryHookABC
 from peek_plugin_base.client.PluginClientEntryHookABC import PluginClientEntryHookABC
 from peek_plugin_base.agent.PluginAgentEntryHookABC import PluginAgentEntryHookABC
 from typing import Type
@@ -7,9 +7,9 @@ from typing import Type
 __version__ = '0.0.0'
 
 
-def peekServerEntryHook() -> Type[PluginServerEntryHookABC]:
-    from .private.server.ServerEntryHook import ServerEntryHook
-    return ServerEntryHook
+def peekLogicEntryHook() -> Type[PluginLogicEntryHookABC]:
+    from .private.server.LogicEntryHook import LogicEntryHook
+    return LogicEntryHook
 
 
 def peekAgentEntryHook() -> Type[PluginAgentEntryHookABC]:
